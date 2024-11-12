3745fa11029d5785371773a9ece010f8f1585bbc
Evan <evan.sherfey@stu.barren.kyschools.us>
2024-11-12 08:23:51 -0600

    Initial Commit

---
+++ Project Milestone/Project_MilestoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_MilestoneApp.swift
+//  Project Milestone
+//
+//  Created by Student on 11/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Project_MilestoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
